eaa51bb9325bb005779f1a42090e6ef2f2a3459f
Taishawnk <taishawnking1@yahoo.com>
2023-11-19 00:43:48 -0600

    Things are really starting to shape up

--- L2workspace/frontend/main.js
+++ L2workspace/frontend/main.js
@@ -4,7 +4,8 @@ import App from './App.vue'
 
 const el = document.getElementById('app')//modifys how the app is running to some dagree had to add this 
 if (el){
-    console.log(el.dataset) //will need to reffrence where I want it render which in ths case is my main.html file just create a div a id of #app
+    const data = {el.dataset}
+    console.log(data) //will need to reffrence where I want it render which in ths case is my main.html file just create a div a id of #app
     createApp(App).mount('#app') //will need to reffrence where I want it render which in ths case is my main.html file just create a div a id of #app
 }//also added this if statement the only reason to do this is some we can add the console.log(el.dataset) data set is tide to the htmle that holds the id=app and allows
 //me if neee to add a data-token ie <div id="app" data-token="1234567890"></div the data token and use it to get the token from the html file. more
