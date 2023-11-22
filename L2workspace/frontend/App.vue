cb546ecba1171559446d4b4e295fcdf007dc4650
Taishawnk <taishawnking1@yahoo.com>
2023-11-21 21:41:35 -0600

    Clean up and code add

--- L2workspace/frontend/App.vue
+++ L2workspace/frontend/App.vue
@@ -1,24 +1,41 @@
 <script setup>
 import TheWelcome from './components/TheWelcome.vue'
+import VueLogo from './assets/Vue.png';
+import { ref } from 'vue'; //re Vue.js instance properties that are used to register or indicate a reference to HTML elements or child elements in the template of your application. 
 const props = defineProps({
   token: String, 
   user: String}) // creats props object and assigns them to the Vue instance passed in token and user from main.js file  ? means it is optional didnt have to pring in defineProps 
   //because it came with  script setup but this is not ture for ever built in vue 
-  console.log(props.token)
+ const token = props.token ? props.token : null  // going to be using this any time a request is made to the backend vue is specal in the fact that we don have to 
+ //jump through alot of loops to get this functionality CSRF token needs to be on every single page for security so very useful to be able to do this in vue so ealily
+ const username = props.user ? props.user : ''
+
+let count = ref(0)
+
+function increment(event){
+  console.log(event)
+  count.value++ 
+}
 </script>
 
 <template>
   <header>
-    
-    {{props.token}}
-    {{props.user}}
-    <div class="wrapper">
-      <HelloWorld msg="You did its!" />
+  
+    {{ count }} - {{token}} - {{username}}
+    <img 
+  :src="VueLogo" 
+  alt="Vue Logo" 
+  class="logo" 
+  width="125" 
+  height="125" 
+  @click="increment">
+    <!--  @click creates clicke events in Vue -->
+    <div class="wrapper"> 
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <TheWelcome  />
   </main>
 </template>
 
--- L2workspace/frontend/main.js
+++ L2workspace/frontend/main.js
@@ -6,7 +6,7 @@ const el = document.getElementById('app')//modifys how the app is running to som
 if (el){
     console.log(el.dataset) //will need to reffrence where I want it render which
     const data = {...el.dataset} //... is 
-    console.log(data) //will need to reffrence where I want it render which in ths case is my main.html file just create a div a id of #app
+     // data will need to reffrence where I want it render which in ths case is my main.html file just create a div a id of #app
     createApp(App, data).mount('#app') //will need to reffrence where I want it render which in ths case is my main.html file just create a div a id of #app
 }//also added this if statement the only reason to do this is some we can add the console.log(el.dataset) data set is tide to the htmle that holds the id=app and allows
 //me if neee to add a data-token ie <div id="app" data-token="1234567890"></div the data token and use it to get the token from the html file. more
